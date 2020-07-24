865c782b5b2e1b747886ff4d99e02c720783c1b2
raisercostin <raisercostin@gmail.com>
2020-07-24 15:23:25 +0300

    display movies

--- components/MoviesList.vue
+++ components/MoviesList.vue
@@ -14,7 +14,7 @@
       ></v-divider>
 
       <v-list-item v-else :key="item.itemTitle">
-        <v-list-item-avatar size=80>
+        <v-list-item-avatar size="80">
           <v-img :src="item.itemImage"></v-img>
         </v-list-item-avatar>
 
@@ -37,47 +37,47 @@ export default {
   }),
   mounted() {
     console.log("Here we call the server for movies!!!");
-    this.items = [
-      { header: "Today" },
-      {
-        itemImage: "https://cdn.vuetifyjs.com/images/lists/1.jpg",
-        itemTitle: "Brunch this weekend?",
-        itemDescription:
-          "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?"
-      },
-      { divider: true, inset: true },
-      {
-        itemImage: "https://cdn.vuetifyjs.com/images/lists/2.jpg",
-        itemTitle:
-          'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
-        itemDescription:
-          "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend."
-      },
-      { divider: true, inset: true },
-      {
-        itemImage: "https://cdn.vuetifyjs.com/images/lists/3.jpg",
-        itemTitle: "Oui oui",
-        itemDescription:
-          "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
-      },
-      { divider: true, inset: true },
-      {
-        itemImage: "https://cdn.vuetifyjs.com/images/lists/4.jpg",
-        itemTitle: "Birthday gift",
-        itemDescription:
-          "<span class='text--primary'>Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?"
-      },
-      { divider: true, inset: true },
-      {
-        itemImage: "https://cdn.vuetifyjs.com/images/lists/5.jpg",
-        itemTitle: "Recipe to try",
-        itemDescription:
-          "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos."
-      }
-    ];
+    // this.items = [
+    //   { header: "Today" },
+    //   {
+    //     itemImage: "https://cdn.vuetifyjs.com/images/lists/1.jpg",
+    //     itemTitle: "Brunch this weekend?",
+    //     itemDescription:
+    //       "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?"
+    //   },
+    //   { divider: true, inset: true },
+    //   {
+    //     itemImage: "https://cdn.vuetifyjs.com/images/lists/2.jpg",
+    //     itemTitle:
+    //       'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
+    //     itemDescription:
+    //       "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend."
+    //   },
+    //   { divider: true, inset: true },
+    //   {
+    //     itemImage: "https://cdn.vuetifyjs.com/images/lists/3.jpg",
+    //     itemTitle: "Oui oui",
+    //     itemDescription:
+    //       "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
+    //   },
+    //   { divider: true, inset: true },
+    //   {
+    //     itemImage: "https://cdn.vuetifyjs.com/images/lists/4.jpg",
+    //     itemTitle: "Birthday gift",
+    //     itemDescription:
+    //       "<span class='text--primary'>Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?"
+    //   },
+    //   { divider: true, inset: true },
+    //   {
+    //     itemImage: "https://cdn.vuetifyjs.com/images/lists/5.jpg",
+    //     itemTitle: "Recipe to try",
+    //     itemDescription:
+    //       "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos."
+    //   }
+    // ];
 
     axios
-      .get("http://localhost:8080/")
+      .get("http://localhost:8000/movies")
       .then(response =>
         // JSON responses are automatically parsed.
         {
